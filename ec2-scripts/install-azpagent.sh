ccb312f98bd2116607677020ee8184a97cde0c32
John Taylor <john@johntaylor.net.au>
2021-01-08 13:47:13 +1100

    initial

---
+++ ec2-scripts/install-azpagent.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+AZP_URL="https://dev.azure.com/jayytee"
+AZP_TOKEN="ap5ocillfloerwz6e6ondwtrsldyt5r5nhf4trfgyg7mvxtzosbq"
+AZP_TOKEN_B64="`echo -n $AZP_TOKEN: | base64`"
+AZP_AGENT_INSTALLDIR="/home/azpagent"
+AZP_AGENT_POOL="MyApp-Production"
+AZP_AGENT_USER="azpagent"
+
+EC2_INSTANCE_ID="`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`"
+
+echo "Downloading Azure Pipelines Agent..."
+case `uname -m` in
+        x86_64)
+                PLATFORM="linux-x64"
+                echo ".. x86_64 architecture detected, using $PLATFORM"
+                ;;
+        *)
+                echo "Could not establish Az Agent platform from processor architecture"
+                ;;
+esac
+
+PACKAGE_URL="$AZP_URL/_apis/distributedtask/packages/agent?platform=$PLATFORM&\$top=1"
+PACKAGES=`curl -s -H "Accept: application/json" -H "Authorization: Basic $AZP_TOKEN_B64" "$PACKAGE_URL"`
+DOWNLOAD_URL=`echo $PACKAGES | python extract-azpagent-downloadurl.py`
+
+
+echo ".. fetching $DOWNLOAD_URL"
+
+if [[ ! -d "$AZP_AGENT_INSTALLDIR" ]]; then
+        mkdir $AZP_AGENT_INSTALLDIR
+fi
+
+cd $AZP_AGENT_INSTALLDIR
+wget -O vsts-agent.tar.gz $DOWNLOAD_URL
+tar -zxf vsts-agent.tar.gz
+
+./config.sh \
+        --unattended \
+        --url $AZP_URL \
+        --auth pat --token $AZP_TOKEN \
+        --pool $AZP_AGENT_POOL \
+        --agent $EC2_INSTANCE_ID \
+        --acceptTeeEula
+
+rm $AZP_AGENT_INSTALLDIR/vsts-agent*.tar.gz
